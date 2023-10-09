ae013a60258bcd940d8b6141d22eaf3c84811ec4
frankvoc <fmvocatura@email.neit.edu>
2023-10-09 16:15:44 -0400

    test.php
    minor fixes.

--- week1/test.php
+++ week1/test.php
@@ -2,13 +2,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css"crossorigin="anonymous">
   <title>Home</title>
 </head>
 <body>  
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"  crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"crossorigin="anonymous"></script>
   <h1>Hello World</h1>
   <div class="list-group">
   <a href="https://github.com/frankvoc/SE266.05" class="list-group-item list-group-item-action active">
@@ -19,7 +20,7 @@
     <p class="mb-1">GitHub Repo Home for SE266.05 Web Development using PHP.</small>
   </a>
   <a href="week1G.php" class="list-group-item list-group-item-action">
-    <div class="d-flex w-100 justify-content-between">
+    <div class="d-flex w-100">
       <h5 class="mb-1">Week 1 Assignment. <i>FizzBuzz</i></h5>
       <small class="text-muted">10/2/2023</small>
     <p class="mb-1">Using PHP to solve FizzBuzz, where if the number is a multiple of 2 it will be Fizz. If the number is a multiple of 3 then it will be Buzz. If the number is a multiple of both it will be FizzBuzz.</p>
@@ -54,7 +55,7 @@
       <h5 class="mb-1">Week 1 Assignment. <i>Fucntions</i></h5>
       <small class="text-muted">10/2/2023</small>
     </div>
-    <p class="mb-1">Using PHP I created a very basic fucntion canEnterClub and passed a persons age through them. If they are 21 or over they can enter(returns true) or under 21(returns false). Then we passed ages through the function and it retuned booleans. These were distinguised by <span>&#9989</span> for being able to enter or a  <span>&#10060</span> for not being able to enter.</p>
+    <p class="mb-1">Using PHP I created a very basic fucntion canEnterClub and passed a persons age through them. If they are 21 or over they can enter(returns true) or under 21(returns false). Then we passed ages through the function and it retuned Booleans. These were distinguished by <span>&#9989</span> for being able to enter or a  <span>&#10060</span> for not being able to enter.</p>
   </a>
 </div>
 </body>
