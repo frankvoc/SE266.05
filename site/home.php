6e52b12e27f3199f9f5298401f4c9d032e500245
frankvoc <fmvocatura@email.neit.edu>
2023-10-10 14:20:42 -0400

    sd

--- site/home.php
+++ site/home.php
@@ -1,12 +1,20 @@
+<?php
+    //create array using array() to define it as an array
+    $array = array("cat", "dog", "mouse");
+    //create an output using implode() function. it takes the ',' as the sepeator inbetween each element in the array.
+    $output = implode (',', $array);
+    //printing output of the array
+    echo $output
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Week 1 C</title>
 </head>
 <body>
-    <h1>Hello</h1>
+    <h1><a href="test.php">Back to Home</a></h1>
 </body>
 </html>
