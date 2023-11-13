3b55b345b6e11fc8281f2c2a1389d651806851ce
frankvoc <fmvocatura@email.neit.edu>
2023-11-13 13:25:42 -0500

    Update to patient

--- patient/model/model_patient.php
+++ patient/model/model_patient.php
@@ -41,24 +41,6 @@
         return ($results);
     }
    
-    // Alternative style to add team records database.
-    function addTeam2 ($team, $division) {
-        global $db;
-        $results = "Not added";
-
-        $stmt = $db->prepare("INSERT INTO teams SET teamName = :team, division = :division");
-       
-        $stmt->bindValue(':team', $team);
-        $stmt->bindValue(':division', $division);
-       
-        if ($stmt->execute() && $stmt->rowCount() > 0) {
-            $results = 'Data Added';
-        }
-       
-        $stmt->closeCursor();
-       
-        return ($results);
-    }
     function searchTeams ($team, $division) {
         global $db;
         $binds = array();
